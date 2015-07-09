3ab7aea614deb6e5fbc85bcc7f3523c383737096
sazarag <sazarag@gmail.com>
2015-07-09 13:03:22 +0300

    repo

---
+++ Repository/Implementations/Repository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Implementations
+{
+    class Repository
+    {
+        #region General
+        #endregion  
+    }    
+}
